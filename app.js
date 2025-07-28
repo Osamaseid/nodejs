f046930423f8bf8ea6567b31803d5e59a4c6ae07
osama seid <osaseid1@gmail.com>
2025-07-28 22:18:05 +0300

    http  module

--- app.js
+++ app.js
@@ -3,8 +3,16 @@ const os  = require('os');
 const fs = require('fs');
 const { text } = require('stream/consumers');
 
-var file = fs.readFileSync( './file.txt', 'utf8')
-    console.log(file);
+var file = fs.readFileSync( './file.txt', 'utf8', (error, data) => {
+    if (error){
+        console.error(error);
+    }
+    else{
+        console.log(data)
+    }
+});
+
+console.log(file);
 
 
 const platfrom = os.platform();
