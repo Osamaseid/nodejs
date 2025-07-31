2ed4c32580b77aefd16ddf29fd66ecef3f3c7b1d
osama seid <osaseid1@gmail.com>
2025-07-31 18:12:55 +0300

    server created

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const http = require('http');
 
+const server = http.createServer(function(req, res){
+  console.log('request recived');
+});
 
-const server = http.createServer((req, res)  () => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-} );
+server.listen(3000, function(){
+  console.log('server is listening on port 3000');
+});
